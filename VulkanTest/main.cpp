c9bbae10572631bde395c47c99fc3c957b30be48
hermann0001 <her.tamilia@stud.uniroma3.it>
2024-10-15 05:17:29 +0200

    added .gitignore

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -14,6 +14,7 @@ int main() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
+
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
